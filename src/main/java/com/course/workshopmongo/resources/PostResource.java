52cab82e6c05471e2d1777653cebd185dd432da9
jplumi <59978477+jplumi@users.noreply.github.com>
2021-11-30 17:25:09 -0300

    Getting post by id

---
+++ src/main/java/com/course/workshopmongo/resources/PostResource.java
@@ -0,0 +1,25 @@
+package com.course.workshopmongo.resources;
+
+import com.course.workshopmongo.domain.Post;
+import com.course.workshopmongo.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/posts")
+public class PostResource {
+
+    @Autowired
+    private PostService service;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Post> findById(@PathVariable String id) {
+        Post post = service.findById(id);
+        return ResponseEntity.ok().body(post);
+    }
+
+}
---
+++ src/main/java/com/course/workshopmongo/service/PostService.java
@@ -0,0 +1,22 @@
+package com.course.workshopmongo.service;
+
+import com.course.workshopmongo.domain.Post;
+import com.course.workshopmongo.repository.PostRepository;
+import com.course.workshopmongo.service.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repository;
+
+    public Post findById(String id) {
+        Optional<Post> optionalPost = repository.findById(id);
+        return optionalPost.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+}
