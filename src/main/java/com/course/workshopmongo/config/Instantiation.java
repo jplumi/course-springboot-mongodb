c3891cf613837e231d7218a715a02bc42438d9de
jplumi <59978477+jplumi@users.noreply.github.com>
2021-11-30 13:35:38 -0300

    Referencing user posts

--- src/main/java/com/course/workshopmongo/config/Instantiation.java
+++ src/main/java/com/course/workshopmongo/config/Instantiation.java
@@ -39,6 +39,10 @@ public class Instantiation implements CommandLineRunner {
         Post post2 = new Post(null, new Date(), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 
         postRepository.saveAll(Arrays.asList(post1, post2));
+
+        maria.getPosts().addAll(Arrays.asList(post1, post2));
+
+        userRepository.save(maria);
     }
 
 }
